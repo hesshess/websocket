c5f581e4274f51382754a784db73715cd11cded0
Hess <hess90wang@gmail.com>
2024-11-25 17:32:28 +0900

    ws: node websocket library implementaion

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-hello;
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", ()=>{
+    console.log(`connected to Server`)
+});
+
+socket.addEventListener("message", (message)=>{
+    console.log('New msg: ', message.data)
+})
+
+socket.addEventListener("close", () =>{
+    console.log("Connected closed")
+})
+
+setTimeout(()=>{
+    socket.send("hello from the browser")
+}, 5000)
--- src/server.js
+++ src/server.js
@@ -1,15 +1,31 @@
+import http from "http";
+import WebSocket from 'ws';
 import express from "express";
 
 const app = express();
-
-const port = 3000
+const port = 3000;
 
 app.set('view engine', "pug");
 app.set('views', __dirname + "/views");
 app.use('/public', express.static(__dirname + "/public"));
 
-app.get('/', (req,res) => res.render("home"));
+app.get('/', (_, res) => res.render("home"));
+app.get("/*", (_, res)=> res.render('/'));
 
-const handleListen = () => console.log(`Listening on port ${port}`)
+const handleListen = () => console.log(`Listening on port ${port}`);
 
-app.listen(port, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server }); 
+
+wss.on("connection", (socket)=>{
+    console.log(`Conneted to Browser`);
+    socket.on("close", ()=>{
+        console.log(`disconnected from browser`);
+    })
+    socket.on("message", message => {
+        console.log(message.toString('utf8'));
+    })
+    socket.send("hello");
+});
+
+server.listen(port, handleListen);
